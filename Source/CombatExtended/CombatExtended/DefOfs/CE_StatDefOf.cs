be7b50eea8adcf006cd5be19adda8c7c5cd99134
Person243546 <72211112+Person243546@users.noreply.github.com>
2026-08-08 14:57:35 +0400

    Add files via upload

--- Source/CombatExtended/CombatExtended/DefOfs/CE_StatDefOf.cs
+++ Source/CombatExtended/CombatExtended/DefOfs/CE_StatDefOf.cs
@@ -50,8 +50,6 @@ public static class CE_StatDefOf
     public static StatDef BodyPartBluntArmor;
     public static StatDef AverageSharpArmor;
     public static StatDef NightVisionEfficiency;
-    public static StatDef ThermalVisionEfficiency;
-    public static StatDef VisibilityConcealmentEfficiency;
 
     public static StatDef SmokeSensitivity;
 
